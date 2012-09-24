b85b8cb15866cd3d659fda75a20de3b2db535868
Paymahn Moghadasian <paymahn1@gmail.com>
2012-09-24 14:11:21 -0500

    Started MSDN Databinding

---
+++ Microsoft Databinding Tutorial/SDKSample/MyData.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.ComponentModel;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SDKSample
+{
+
+	class MyData : INotifyPropertyChanged
+	{
+		private string _colorName = "red";
+
+		public string ColorName
+		{
+			get { return _colorName; }
+			set
+			{
+				if (_colorName != value)
+				{
+					_colorName = value;
+					NotifyPropertyChanged("ColorName");
+				}
+			}
+		}
+
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		public void NotifyPropertyChanged(string propertyName)
+		{
+			if (PropertyChanged != null)
+			{
+				PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+			}
+		}
+	}
+}
